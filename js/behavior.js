2d822f23a238fe1349d84e3d0f2a8497dd29e769
SuperbWorld0101 <13280489639@163.com>
2025-03-05 15:52:23 +0800

    create foundation

--- js/behavior.js
+++ js/behavior.js
@@ -17,3 +17,12 @@ sub.addEventListener('click',function(){
 
 
 
+$('[data-rating] .star').on('click', function() {
+  var selectedCssClass = 'selected';
+  var $this = $(this);
+  $this.siblings('.' + selectedCssClass).removeClass(selectedCssClass);
+  $this
+    .addClass(selectedCssClass)
+    .parent().addClass('is-voted');
+});
+
