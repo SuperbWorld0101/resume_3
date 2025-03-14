a59182f1bb21483a387d29a0b258b490614fd4d8
SuperbWorld0101 <13280489639@163.com>
2025-03-14 20:08:11 +0800

    add js to app.js

--- js/app.js
+++ js/app.js
@@ -17,3 +17,12 @@ var sub = document.getElementById('submit');
 sub.addEventListener('click',function(){
   alert("Thank you again!");
 });
+
+$('[data-rating] .star').on('click', function() {
+  var selectedCssClass = 'selected';
+  var $this = $(this);
+  $this.siblings('.' + selectedCssClass).removeClass(selectedCssClass);
+  $this
+    .addClass(selectedCssClass)
+    .parent().addClass('is-voted');
+});
